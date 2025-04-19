955f444073d775ae7bba549166530d92372ed5f5
dodurga71 <78793327+dodurga71@users.noreply.github.com>
2025-04-20 00:25:42 +0300

    finance_engine.py

--- engines/finance_engine.py
+++ engines/finance_engine.py
@@ -1,5 +1,2 @@
-def calculate_quantum_shift():
-    return 'Quantum kayması'
-
-def run_quantum_analysis():
-    return calculate_quantum_shift()
+def calculate_finance():
+    return "Finance calculated successfully."
