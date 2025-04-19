b3c6d3516b5f19dce36b419b2e2f46c160fa961d
dodurga71 <78793327+dodurga71@users.noreply.github.com>
2025-04-19 23:12:07 +0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from fastapi import FastAPI
 from energy_engine import calculate_energy_flow
 from finance_engine import calculate_finance
 from predict_service import make_prediction
 from quantum_engine import run_quantum_analysis
 from astro_engine import generate_astro_report
+from routes.status import router as status_router
+from routes.predict import router as predict_router
 
 app = FastAPI(
     title="SENKRON AI Core V1",
@@ -14,3 +17,4 @@ app = FastAPI(
 
 app.include_router(status_router, prefix="/senkron")
 app.include_router(predict_router, prefix="/senkron")
+
